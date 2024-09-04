c2c4efdbbd256aaee3306a45a31d4240bd9a5649
Bogdan Brock <bogdan.brock@yandex.ru>
2024-09-04 18:14:37 +0300

    edit_project

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True) == 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
 
 # Application definition
